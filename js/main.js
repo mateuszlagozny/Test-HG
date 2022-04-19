114d1f7c4437cfe34568b97876acae1c06d96414
mlagozny <mateusz.lagozny1@gmail.com>
2022-04-19 03:26:12 +0200

    01

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 const input = document.querySelectorAll("input");
-console.log(input);
-
-
 
 input.forEach(function(item){
 
@@ -11,20 +8,33 @@ input.forEach(function(item){
         } else {
             console.log("Użytkownik nie zaznaczył odpowiedzi.")
         }
-    })
 
-console.log(item)
+    })
 
 });
 
+function change() {
+    if (document.getElementById('firstChbx').checked == true ||
+        document.getElementById('secondChbx').checked == true ||
+        document.getElementById('thirdChbx').checked == true)
+        document.getElementById('firstChbx').checked = false,
+            document.getElementById('secondChbx').checked = false,
+            document.getElementById('thirdChbx').checked = false;
+    
+    return
+};
 
+const btn = document.querySelector('button');
 
-
-
-
-
-
-
-
-
-
+btn.addEventListener('click', function () {
+    if (
+        document.getElementById('firstChbx').checked == false &&
+        document.getElementById('secondChbx').checked == false &&
+        document.getElementById('thirdChbx').checked == false &&
+        document.getElementById('fourthChbx').checked == false
+    ) {
+        alert('Proszę zaznaczyć odpowiedź!')
+    } else {
+        alert('Dziękuję za wypełnienie ankiety :)')
+    }
+});
